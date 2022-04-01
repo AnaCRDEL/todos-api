16866430af9a679b895e8199f0ff05217f359819
Ana Carolina R Lima <anacarolina.rdel@gmail.com>
2022-03-31 21:04:47 -0300

    fix cors config

--- app.js
+++ app.js
@@ -8,12 +8,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(
-    cors({
-        credentials: true,
-        origin: ['http://localhost:3000']
-    })
-);
+app.use(cors());
 
 app.use('/auth', require('./routes/auth.routes'));
 
